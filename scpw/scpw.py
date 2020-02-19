153c1ed239cb1b5d2f4ac5304b6562dcb08761d9
lcerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2020-02-19 22:56:53 +0100

    first scpw commit

---
+++ scpw/scpw.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+author: Lorenzo Cerrone
+"""
+
+def main():
+    print("Hello! I am the smallest conda package in the West")
+
+
+if __name__ == "__main__":
+    x = main()
